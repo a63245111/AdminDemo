ba8b30445afbda28e33e80c851ea1dda1dd87b75
KLAY\ACS <2330289253@qq.com>
2019-01-18 10:18:05 +0800

    demo

---
+++ Admin.Data/Models/User/AdminUser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Admin.Data.User
+{
+    /// <summary>
+    /// 管理员
+    /// </summary>
+    public class AdminUser
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string PassWord { get; set; }
+        /// <summary>
+        /// 权限
+        /// </summary>
+        public int RoleId { get; set; }
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        public int Creator { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime? CreateTime { get; set; }
+    }
+}
