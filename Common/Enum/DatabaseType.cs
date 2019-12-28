06bfb6b384db55549718c49dea099abbebc86d0b
Klay <2330289253@qq.com>
2019-12-28 10:42:48 +0800

    修改

---
+++ Common/Enum/DatabaseType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Enum
+{
+    /// <summary>
+    /// 数据库类型
+    /// </summary>
+    public enum DatabaseType
+    {
+        /// <summary>
+        /// Sql Server数据库
+        /// </summary>
+        SqlServer,
+        /// <summary>
+        /// MySql数据库
+        /// </summary>
+        MySql,
+        /// <summary>
+        /// PostgreSQL数据库
+        /// </summary>
+        PgSql,
+        /// <summary>
+        /// Oracle数据库
+        /// </summary>
+        Oracle
+    }
+}
